761a9e6f692163468932df661e067b90e0145242
Alisson Morais <alissonfpmorais@gmail.com>
2022-10-06 14:11:07 -0300

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ChangesetExtras.MixProject do
   def project do
     [
       app: :changeset_extras,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       description: description(),
